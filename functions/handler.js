4f3e2aa247ab6234618c9a0d1f34b9c768adddea
nyw2020 <nyw@amazon.com>
2022-05-27 18:56:22 -0400

    webpack is op

--- functions/handler.js
+++ functions/handler.js
@@ -1,5 +1,6 @@
 const AWS = require("aws-sdk");
 const express = require("express");
+const res = require("express/lib/response");
 const serverless = require("serverless-http");
 
 const app = express();
@@ -58,11 +59,14 @@ app.post("/users", async function (req, res) {
   }
 });
 
+app.get("/webpack", async function (req, res) {
+  res.status(200).json({ body: "hello, webpack!" });
+});
+
 app.use((req, res, next) => {
   return res.status(404).json({
     error: "Not Found",
   });
 });
-
 
 module.exports.handler = serverless(app);
